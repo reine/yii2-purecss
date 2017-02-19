2d31ff667ffcbc2c95233b6e9de384b3838542df
Reina Sison <reawakened.phoenix@gmail.com>
2017-02-19 17:26:49 +0800

    Added regular and non-responsive rollups.

--- src/PureNonResponsiveAssets.php
+++ src/PureNonResponsiveAssets.php
@@ -5,13 +5,13 @@ namespace Reine\PureCSS
 use yii\web\AssetBundle;
 
 /**
- * Pure CSS Responsive Rollup
+ * Pure CSS Non-Responsive Rollup
  */
 class PureAssets extends AssetBundle
 {
 	public $sourcePath = '@vendor/reine/yii2-purecss/src';
 	public $css = [
-		'css/pure-min.css',
+		'css/pure-nr-min.css',
 	];
 	public $js = [];
 	public $depends = [];
